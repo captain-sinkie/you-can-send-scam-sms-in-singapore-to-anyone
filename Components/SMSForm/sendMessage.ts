33cf16037794208e8d028d836fcead454e7eedd9
Captain Sinkie <captain.sinkie@gmail.com>
2022-01-24 15:08:46 +0800

    put alert msg for live

--- Components/SMSForm/sendMessage.ts
+++ Components/SMSForm/sendMessage.ts
@@ -1,6 +1,8 @@
 export const sendLiveSMSMessage = () => {
   // decide who to use to send
   // alert(burstSMSApiUrl({ message: 'test', to: '123', from: '321' }))
+
+  alert('sent from live')
 }
 
 export const sendPreviewSMSMessage = () => {
